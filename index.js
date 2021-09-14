9a255f5e4c8813154b793eb6a846bcee22571184
robigan <35210888+robigan@users.noreply.github.com>
2021-09-14 10:48:29 +0200

    First commit

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-chrome.runtime.onInstalled.addListener(() =>  {
-    // 1 - SAVING something on "chrome.storage"
-    // chrome.storage.sync.set({color: '#3aa757'}, () {
-    //   console.log('The color is green.');
-    // });
-  
-    // 2 - Making the popup avaiable
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
-        chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-            // pageUrl: {hostEquals: 'developer.chrome.com'},
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-        }]);
-    });
-
-});
+// Code of my exstenssion
--- popup/popup.js
+++ popup/popup.js
@@ -1,8 +1,13 @@
 chrome.tabs.query({active: true, currentWindow: true}, (tabs) =>  {
     chrome.tabs.insertCSS(
         tabs[0].id,
-        {file: 'page/style.css'});
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {file: 'page/script.js'});
+        {file: 'style.css'});
+    // chrome.tabs.executeScript(
+    //     tabs[0].id,
+    //     {file: 'assets/jQuery.js'}, 
+    //     function() {
+    //         chrome.tabs.executeScript(
+    //             tabs[0].id,
+    //             {file: 'assets/script.js'});
+    //     });
 });
