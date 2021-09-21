0c55f6c2c059d1d1ed8c0bab69705661a25d0930
robigan <35210888+robigan@users.noreply.github.com>
2021-09-21 11:26:44 +0200

    Revert file popup.js to commit 7b13ba

--- popup/popup.js
+++ popup/popup.js
@@ -2,12 +2,7 @@ chrome.tabs.query({active: true, currentWindow: true}, (tabs) =>  {
     chrome.tabs.insertCSS(
         tabs[0].id,
         {file: 'page/style.css'});
-    // chrome.tabs.executeScript(
-    //     tabs[0].id,
-    //     {file: 'assets/jQuery.js'}, 
-    //     function() {
-    //         chrome.tabs.executeScript(
-    //             tabs[0].id,
-    //             {file: 'assets/script.js'});
-    //     });
+    chrome.tabs.executeScript(
+        tabs[0].id,
+        {file: 'page/script.js'});
 });
