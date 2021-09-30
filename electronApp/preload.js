357525c1152cfa58b75ea1a6e8a61e7e28d70774
robigan <35210888+robigan@users.noreply.github.com>
2021-09-30 10:33:29 +0200

    :O

--- electronApp/preload.js
+++ electronApp/preload.js
@@ -1,11 +1,12 @@
 const { resolve } = require("path");
+const development = process.env.NODE_ENV === "development" || false;
 
 const TO_HIDE = ["header#main-header", "div.secondary-nav.nav-learning-platform", "footer.et-l.et-l--footer"];
 const TO_FIX = "div#page-container";
 
 const patcher = () => {
     if (location.host === "www.managebac.com", location.pathname === "/login") {
-        console.log("Applying patch script");
+        development ? console.log("Applying patch script") : undefined;
         TO_HIDE.forEach((query) => document.querySelector(query).style.display = "none");
         (async () => {
             setTimeout(() => document.querySelector(TO_FIX).style.paddingTop = "0px", 300);
